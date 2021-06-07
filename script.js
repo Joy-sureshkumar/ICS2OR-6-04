6728bee511e45a81fbfeec2f782432c5fdae4c03
Joy Sureshkumar <>
2021-06-07 15:08:28 +0000

    Changes

--- script.js
+++ script.js
@@ -7,12 +7,12 @@ document.getElementById('button').addEventListener('click', area)
 
 function area () {
   a = document.getElementById('a').value
- a = parseInt(a)
+  a = parseInt(a)
   b = document.getElementById('b').value
- b = parseInt(b)
+  b = parseInt(b)
   h = document.getElementById('h').value
- h = parseInt(h)
-  result = (a + b)/2 *h
+  h = parseInt(h)
+  result = (a + b) / 2 * h
 
-alert(result)
+  alert(result)
 }
